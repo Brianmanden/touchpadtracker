4a9e86470a1c0065e56e87a6f7beb8783175dfac
Brian Andersen <brianmanden@gmail.com>
2016-12-25 22:39:17 +0100

    minor changes

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 	var path = require('path');
 	var server = require('http').Server(app);
 	var io = require('socket.io')(server);
-	
+	var robot = require('robotjs');
+	robot.setMouseDelay(2);
+
 	app.get('/', function(req, res){
 		res.sendFile(path.join(__dirname + '/views/index.html'));
 	});
@@ -13,18 +15,16 @@
 	});
 	
 	io.on('connection', function(socket){
+		console.log("connected");
 		socket.on('mouseTracked', function(data){
-			//console.dir(data);
-			var res = socket.emit('dataLogged', { hello: 'world'} );
-			console.log(data);
+			//var res = socket.emit('dataLogged', { hello: 'world'} );
+			console.dir(data);
+			//robot.moveMouse(data.touchX, data.touchY);
 		});
 	});
 	
 	server.listen(process.env.PORT || 3000, function(){
 		console.log("server IP address: " + server.address());
-		console.dir(server);
-		console.log("server running on port: " + process.env.PORT || 3000);
-		console.log('opened server on', server.address().address);
 	});
 	
 })();
