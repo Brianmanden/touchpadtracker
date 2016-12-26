8695139267965a05ae38935103f31765a2bd5abd
Brian Andersen <brianmanden@gmail.com>
2016-12-26 14:09:20 +0100

    someThing

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 (function(){
+	'use strict';
 	var app = require('express')();
 	var path = require('path');
 	var server = require('http').Server(app);
 	var io = require('socket.io')(server);
 	var robot = require('robotjs');
+
 
 	robot.setMouseDelay(0);
 
@@ -16,6 +18,10 @@
 	});
 	
 	io.on('connection', function(socket){
+		console.log('connection');
+		console.log('centering mouse on screen');
+		//robot.moveMouse();
+
 		socket.on('pointerPosition', function(data){
 			robot.moveMouse(data.pointerPosition.x, data.pointerPosition.y);
 		});
@@ -27,6 +33,7 @@
 	
 	server.listen(process.env.PORT || 3000, function(){
 		console.log('server IP address: ' + server.address());
+		console.dir(server);
 	});
 	
 })();
