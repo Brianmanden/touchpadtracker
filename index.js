a1ca41c6ec8044b72249ef88fd5e4c3ad4bced53
Brian Andersen <brianmanden@gmail.com>
2016-12-26 16:45:18 +0100

    some refactoring mayn

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
 (function(){
 	'use strict';
-	var app = require('express')();
+
+	var express = require('express');
+	var app = express();
 	var path = require('path');
 	var server = require('http').Server(app);
 	var io = require('socket.io')(server);
 	var robot = require('robotjs');
+	var hostScreen = robot.getScreenSize();
 
+	app.set('view options', { layout: false } );
+	app.use(express.static( __dirname + '/views' ) );
 
 	robot.setMouseDelay(0);
 
 	app.get('/', function(req, res){
-		res.sendFile(path.join(__dirname + '/views/index.html'));
+		res.render('/views/index');
 	});
 
-	app.get('/log', function(req, res){
-		res.sendFile(path.join(__dirname + '/views/log.html'));
-	});
-	
 	io.on('connection', function(socket){
 		console.log('connection');
 		console.log('centering mouse on screen');
-		//robot.moveMouse();
+		centerPointer(hostScreen);
 
 		socket.on('pointerPosition', function(data){
 			robot.moveMouse(data.pointerPosition.x, data.pointerPosition.y);
 		});
 
 		socket.on('buttonClick', function(data){
-			robot.mouseClick(data.buttonPress);
+			if(data.buttonPress == 'center'){
+				centerPointer(hostScreen);
+			}else{
+				robot.mouseClick(data.buttonPress);
+			}
 		});
 	});
-	
+
+	function centerPointer(hostScreen){
+		robot.moveMouse(hostScreen.width/2, hostScreen.height/2);
+	}
+
 	server.listen(process.env.PORT || 3000, function(){
-		console.log('server IP address: ' + server.address());
-		console.dir(server);
+		console.log('Server IP address: ' + server.address());
+		console.log();
 	});
 	
 })();
